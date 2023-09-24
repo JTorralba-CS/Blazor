8cf74c4da6c22151a96b47caf02661c33e625da7
Julius Torralba <Julius.Torralba@GMail.com>
2023-09-23 18:15:47 -0600

    Implement client connect/disconnect messages.

--- WASM/Server/Hubs/ChatHub.cs
+++ WASM/Server/Hubs/ChatHub.cs
@@ -5,6 +5,32 @@ namespace WASM.Server.Hubs;
 
 public class ChatHub : Hub
 {
+    public override Task OnConnectedAsync()
+    {
+        Message _Message = new Message();
+
+        _Message.Time = DateTime.Now;
+        _Message.User = "SIGNALR";
+        _Message.Content = Context.ConnectionId.ToString().ToUpper().Substring(0, 5) + " connected.";
+
+        Clients.All.SendAsync("RX", _Message);
+
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        Message _Message = new Message();
+
+        _Message.Time = DateTime.Now;
+        _Message.User = "SIGNALR";
+        _Message.Content = Context.ConnectionId.ToString().ToUpper().Substring(0, 5) + " disconnected.";
+
+        Clients.All.SendAsync("RX", _Message);
+
+        return base.OnDisconnectedAsync(exception);
+    }
+
     public Task TX(Message _Message)
     {
         if (_Message.Content.ToUpper().Trim() == "~")
